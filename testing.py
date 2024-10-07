3f9188faee67c58ac031bfaad1c3f6e2da011706
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2024-10-07 11:09:53 +0700

    user

--- testing.py
+++ testing.py
@@ -1,2 +1,23 @@
+#1
 print("Refky Muhammad Syahrin")
+print ("\n")
+
+#2
 print("I am learning Python basics.")
+
+#3
+user = input("Siapa nama kamu: ")
+
+print ("Hello , ", user)
+
+#4
+#User adalah variabelnya
+#Input adalah function untuk memasukan data
+#Print command untuk output di dalam python
+
+#5
+age = input("Berapa umur kamu? ")
+print(age)
+#Program ini menanyakan usia pengguna dan kemudian menampilkannya.
+#Program pertama-tama meminta pengguna untuk memasukkan usia mereka,
+#dan kemudian mencetak input yang diberikan oleh pengguna.
